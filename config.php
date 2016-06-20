dc059d0f212f0b8a601b47ebe38653a4fa33bddb
Adam Schaub <adam.m.schaub@gmail.com>
2016-06-20 10:42:37 -0700

    updated database configuration

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+  /* site config */
+  $site_url = "https://id-spoons.herokuapp.com";
+  $site_password = "asdf1234";
+  
+  /* database config */
+  $db_host = "us-cdbr-iron-east-04.cleardb.net";
+  $db_port = 8889;
+  $db_user = "b61e5d6834f4bd";
+  $db_pass = "ca5613b8";
+  $db_name = "heroku_222c6088385dcee";
+  
+  $timezone_number = "-4:00";
+  $timezone_name = "America/New_York";
+  
+  date_default_timezone_set($timezone_name);
+?>
